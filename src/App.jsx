533df04f8ec0cb8bed0c4694253dc5b1cca5dd27
icaro <icarosaragao2003@gmail.com>
2025-11-24 16:58:28 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FormLogin from './components/FomLogin';
+
+function App() {
+  return (
+    <>
+      <FormLogin />
+    </>
+  );
+}
+
+export default App;
